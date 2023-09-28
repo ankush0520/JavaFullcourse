1decea80e7510fe6d967bc7d9c2bc8ff2fa18092
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-29 01:04:42 +0530

    strings are immutable

---
+++ string1.java
@@ -0,0 +1,16 @@
+package JavaFullcourse;
+
+public class string1 {
+    public static void main(String args[])
+    {
+        String str="Ankush"; //immutable(cann't change)
+        str.concat("Kumar"); // it dosen't concate
+        String str2=str.concat("Kumar");  //we can change string by the use of duplica string
+        System.out.println(str);
+        System.out.println(str2);
+/*beneft of string immutable;
+ 1. save memory;
+ 2. fast executable;*/
+ }
+    
+}
