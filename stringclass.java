297ba8cb5009105996ad9ea8e595072cc6e08b9d
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-29 00:41:27 +0530

    Basics of Strings

---
+++ stringclass.java
@@ -0,0 +1,26 @@
+package JavaFullcourse;
+
+public class stringclass {
+    public static void main(String args[])
+    {
+        String str="Ankush Kumar";
+        String str1=new String("Ankush Kumar");
+        int l=str.length();//for finding length of string
+        System.out.println(l);
+        System.out.println(str.toUpperCase()); // convert all letter to upper case
+        System.out.println(str.toLowerCase()); // convert all letter to lower case
+        if(str==str1)
+        {
+           System.out.println("Both are equals");
+        }
+        else
+        {
+          System.out.println("Not Equals");
+        }
+        if(str.equals(str1))
+        {
+            System.out.println("Equals");
+        }
+    }
+    
+}
