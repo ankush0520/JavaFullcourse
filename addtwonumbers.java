0007a0b4cff7ee3761543cb9f1b6ab18bef4500b
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-28 17:17:12 +0530

    basics

---
+++ addtwonumbers.java
@@ -0,0 +1,16 @@
+package JavaFullcourse;
+import java.util.Scanner;
+class add{
+    public static void main(String args[])
+    {
+       int a,b,c;
+       System.out.println("Enter any two numbers");
+       Scanner sc =new Scanner(System.in);
+       a=sc.nextInt();
+       b=sc.nextInt();
+       c=a+b;
+       System.out.println("sum of numbers are:" +c);
+       sc.close();
+
+    }
+}
---
+++ forloop.java
@@ -0,0 +1,17 @@
+package JavaFullcourse;
+import java.util.Scanner;
+class for_loop{
+    public static void main(String args[])
+    {
+        int num;
+        System.out.println("Enter any number");
+        Scanner obj=new Scanner(System.in);
+        num=obj.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println (num*i);
+        }
+
+       obj.close();
+    }
+}
