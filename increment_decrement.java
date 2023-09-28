341ba88b84f724cd7c4558d3fe621963ced0cd9a
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-28 21:58:13 +0530

    pre-post increment/decrement

---
+++ increment_decrement.java
@@ -0,0 +1,13 @@
+package JavaFullcourse;
+
+public class increment_decrement{
+    public static void main(String args[])
+    {
+        int a=10;
+        System.out.println(a--); //10   --->9
+        System.out.println(--a); //8   ---->8
+        System.out.println(a++); //8    ---->9
+        System.out.println(++a);  //10
+    }
+    
+}
