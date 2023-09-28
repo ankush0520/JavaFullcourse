bec88cf33f035d54b614af6319283713285d64e2
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-28 18:06:22 +0530

    whileloop

---
+++ whileloop.java
@@ -0,0 +1,28 @@
+package JavaFullcourse;
+
+import java.util.Scanner;
+
+public class whileloop {
+    public static void main(String args[])
+    {
+        int num;
+        System.out.println("Enter any number");
+        Scanner s=new Scanner(System.in);
+        num=s.nextInt();
+        while(num>=0)
+        {
+            if(num%2==0){
+                System.out.println("Even Number");
+                break;
+
+            }
+            else{
+                      System.out.println("Odd number");
+                      break;
+            }
+        }
+        System.out.println("While loop ended.......");
+         s.close();
+    }
+   
+}
