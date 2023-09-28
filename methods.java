04f7422b09a1f18681d1580caee535245d2daba6
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-28 23:07:18 +0530

    Methods in Java

---
+++ methods.java
@@ -0,0 +1,32 @@
+/*package JavaFullcourse;
+
+public class methods {
+    public static void main(String args[])
+    {
+       int ans= sum(10,20);//method calling;
+       System.out.println("Addition=" +ans);
+
+    }
+    static int sum(int a,int b)//method definition
+    {
+         return a+b;
+    }
+}*/
+
+//method without parameter with no return
+
+package JavaFullcourse;
+
+public class methods {
+    public static void main(String args[])
+    {
+        methods m=new methods();
+       m.sum();//method calling;
+       
+
+    }
+    void sum()//method definition
+    {
+          System.out.println("Ankush");
+    }
+}
