5d4ba44df4871d261129591bad46feaefe373337
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-28 22:06:15 +0530

    Ternary Operator

---
+++ ternary.java
@@ -0,0 +1,11 @@
+package JavaFullcourse;
+
+public class ternary {
+    public static void main(String args[])
+    {
+        int a=10,b=20,c=5;
+       int ans= (a>b)?(a>c?a:c):(b>c?b:c);
+       System.out.println(ans);
+    }
+    
+}
