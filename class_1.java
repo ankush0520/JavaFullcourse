1c3cf6af90e668faae175624aa038cd58e346bb6
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-29 13:26:32 +0530

    Simple class and objects

---
+++ class_1.java
@@ -0,0 +1,26 @@
+package JavaFullcourse;
+
+public class person{
+    int age=21;
+    int weight=70;
+    String color="Light";
+
+    void eat()
+    {
+        System.out.println("I am eating");
+    }
+    void sleep()
+    {
+        System.out.println("I am sleeping");
+    }
+    public static void main(String args[])
+    {
+
+        person P=new person();
+        System.out.println(P.age);
+        System.out.println(P.weight);
+        System.out.println(P.color);
+        P.eat();
+        P.sleep();
+    }
+}
