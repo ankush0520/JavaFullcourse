ab8813e7b55806ba5cd590a017bc71f9eb16f25d
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-29 20:51:38 +0530

    this keyword is calling parameterized constructor from default constructor

---
+++ this3.java
@@ -0,0 +1,18 @@
+class A{
+
+	A()
+	{
+      this(25);//here this keyword is calling parameterized constructor from default constructor
+	}
+	A(int a)
+	{
+		
+        System.out.println(a);
+	}
+	
+	public static void main(String[] args)
+	{
+		 A ref=new A();
+		
+	}
+}
