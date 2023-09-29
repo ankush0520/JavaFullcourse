d94e9f65f40c9bc1d3997440a48ff5d08fcb64a1
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-29 19:16:16 +0530

    super keyword for variable

---
+++ super.java
@@ -0,0 +1,20 @@
+class A{
+	int a=10;
+}
+class B extends A{
+
+   int a=20;
+   void show()
+   {
+   	
+   	System.out.println(a);
+   	System.out.println(super.a);
+   }
+}
+class Test{
+	public static void main(String[] args)
+	{
+		B c=new B();
+		c.show();
+	}
+}
