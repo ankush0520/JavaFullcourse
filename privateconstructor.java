bf9c3433cf79b6a78cecf6bf7d9fbfd842cd5f6c
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-29 18:48:25 +0530

    Private Constructor

---
+++ privateconstructor.java
@@ -0,0 +1,16 @@
+class A{
+	int a;
+	double b;
+	String c;
+	private A()//private constructor is not accessed outside the class
+	{
+		a=10;
+		b=30.99;
+		c="Ankush";
+	     System.out.println(a+" "+b+" "+c);
+	}
+	public static void main(String[] args)
+	{
+		A ans=new A();
+	}
+}
