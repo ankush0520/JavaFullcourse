4d5b274d23c629c5d1cdce6fc276cd4d190d80cb
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-29 17:45:32 +0530

    parametrized constructor

---
+++ parametrized.java
@@ -0,0 +1,24 @@
+class A{
+	int x,y;
+	A(int a,int b)//parametrized constructor
+	{
+	  x=a;
+	  y=b;
+
+	}
+	A(int a,String b)//parametrized constructor
+	{
+	  System.out.println(a+" "+b);
+	}
+	void show(){
+	System.out.println(x+" "+y);
+	}
+}
+class B{
+	public static void main(String args[])
+	{
+	 A ans=new A(100,200);
+	 ans.show();
+	 A ref=new A(10,"Ankush");
+	}
+}
