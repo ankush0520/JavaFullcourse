c0b9b90d558697023b483b9e2d9edad1b08bf94b
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-29 17:35:33 +0530

    default constructor

---
+++ default.java
@@ -0,0 +1,22 @@
+class A{
+	int a;
+	String b;
+	boolean c;
+	A()//default constructor
+	{
+	  a=25;
+	  b="Ankush";
+	  c=true;
+
+	}
+	void show(){
+	System.out.println(a+" "+b+" "+c);
+	}
+}
+class B{
+	public static void main(String args[])
+	{
+	 A ans=new A();
+	 ans.show();
+	}
+}
