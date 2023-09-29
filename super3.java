4d7e4a81250be82c766ddc39f40978a4cf402d70
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-29 19:38:04 +0530

    super keyword for parameterized constructor

---
+++ super3.java
@@ -0,0 +1,20 @@
+class A{
+	A(int a)
+	{
+		System.out.println("Hello world"+ a);
+	}
+}
+class B extends A{
+
+   B()
+   {
+   	super(20); //in the case of parameterized constructor it is necessary to write super keyword for access super class constructor
+   	System.out.println("Hello Ankush");
+   }
+}
+class Test{
+	public static void main(String[] args)
+	{
+		B c=new B();
+	}
+}
