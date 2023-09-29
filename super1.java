a98feb19bde7cf4a1558756ca2da0f0a19b75000
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-29 19:20:28 +0530

    super keyword for method

---
+++ super1.java
@@ -0,0 +1,22 @@
+class A{
+	void show()
+	{
+		System.out.println("Hello world");
+	}
+}
+class B extends A{
+
+   void show()
+   {
+   	
+    super.show();//for super class method
+   	System.out.println("Hello Ankush");
+   }
+}
+class Test{
+	public static void main(String[] args)
+	{
+		B c=new B();
+		c.show();
+	}
+}
