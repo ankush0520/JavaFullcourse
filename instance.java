298d328bed9c8b5c6b788bd569d52782177f8d97
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-30 11:02:19 +0530

    Instance Block

---
+++ instance.java
@@ -0,0 +1,16 @@
+class A{
+
+A()
+{
+	System.out.println("Default Constructor");
+}
+{//instance blocks
+ System.out.println("Instance block");//instance block is called before default constructor
+}
+public static void main(String[] args)
+{
+	A ref=new A();
+}
+
+
+}
