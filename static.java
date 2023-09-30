fee90035551ea4a8923d21b16ca379052cd1e403
ankush0520 <guptaankushkumar164@gmail.com>
2023-09-30 11:23:17 +0530

    Static Block

---
+++ static.java
@@ -0,0 +1,12 @@
+class A{
+
+static int a=10;
+static{//instance blocks
+ System.out.println("staic block"+" "+ a);//static block is only used for static variable
+}
+public static void main(String[] args)
+{
+	}
+
+
+}
