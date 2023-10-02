30c9bff9a3086eb3881fdb01bb0b63b0a7578e48
ankush0520 <guptaankushkumar164@gmail.com>
2023-10-02 22:49:31 +0530

    Multiple Inheritance through Interface

---
+++ inheritance2.java
@@ -0,0 +1,27 @@
+          /*Multiple Inheritance through Interface*/
+
+interface A
+{
+	void Show(); //public+abstract
+}
+interface B
+{
+	void Disp(); //public+abstract
+}
+class Multiple implements A,B
+{
+  public void Show()
+  {
+  	System.out.println("Interface A");
+  }
+  public void Disp()
+  {
+  	System.out.println("Interface B");
+  }
+  public static void main(String[] args)
+  {
+  	Multiple m=new Multiple();
+  	m.Show();
+  	m.Disp();
+  }
+}
