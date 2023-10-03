8620abcc2bbfe8ae6e466e3af9c953de2efd5eee
ankush0520 <guptaankushkumar164@gmail.com>
2023-10-03 12:26:41 +0530

    Compile time polymorphism(Method Overloading)

---
+++ polymorphism.java
@@ -0,0 +1,32 @@
+                      /* Compile Time Polymorphism */
+                      /* Method Overloading */
+
+class A
+{
+	int add()
+	{
+		int a=10,b=20,c;
+		c=a+b;
+		return c;
+	}
+	void add(int x,int y)
+	{
+		int c;
+		c=x+y;
+		System.out.println(c);
+	}
+	void add(int x,double y)
+	{
+		double c;
+		c=x+y;
+		System.out.println(c);
+	}
+	public static void main(String[] args)
+	{
+		A r=new A();
+		int add=r.add();
+		r.add(100,200);
+		r.add(50,44.34);
+		System.out.println(add);
+	}
+}
