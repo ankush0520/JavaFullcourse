5192b4516ff1a8dbd89de0d4e72defa9f6375c05
ankush0520 <guptaankushkumar164@gmail.com>
2023-10-04 16:23:56 +0530

    File Handling Program for reading data from File

---
+++ File3.java
@@ -0,0 +1,28 @@
+import java.io.*;
+class fileReader
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			FileReader r=new FileReader("C:\\Users\\hp\\OneDrive\\Desktop\\ank.txt");
+			try
+			{
+               int i;
+               while((i=r.read())!=-1)
+               {
+               	System.out.print((char)i);
+               }
+			}
+			finally
+			{
+               r.close();
+               System.out.print("File Closed");
+			}
+		}
+		catch(IOException e)
+		{
+		   System.out.println("Exception Handled...!");
+		}
+	}
+}
