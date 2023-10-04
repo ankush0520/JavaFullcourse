9d7ae770ec1cc67693faedf4f652809d13ca672f
ankush0520 <guptaankushkumar164@gmail.com>
2023-10-04 10:32:41 +0530

    Exception Handling

---
+++ Exception.java
@@ -0,0 +1,20 @@
+class Test
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Main method statred");
+		int a=10,b=0,c;
+        try
+        {
+        	c=a/b; // c=10/0;
+		    System.out.println(c);
+        }
+        catch(Exception e)  //we can write ArithmeticException  instead of Exception because it is arithmetic exception
+        { 
+
+		 System.out.println(e);
+        }
+
+		System.out.println("Main method ended");
+	}
+}
