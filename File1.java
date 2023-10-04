68edcd797050b881e85aa11e8cb1f6ffbf995f7c
ankush0520 <guptaankushkumar164@gmail.com>
2023-10-04 12:06:22 +0530

    File Handling Program for open a file

---
+++ File1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+class createFile
+{
+  public static void main(String[] args) // we can remove try and catch block and write public static void main(String[] args) throws IOException
+  {
+		File f=new File("C:\\Users\\hp\\OneDrive\\Desktop\\ank.txt");
+	try
+	{
+		if(f.createNewFile())
+		{
+			System.out.println("File Successfully Create....!");
+		}
+		else
+		{
+			System.out.println("File Already Exist....!");
+		}
+	}
+	catch(IOException i)
+	{
+		System.out.println("Exception Handled....!");
+	}
+  }
+}
