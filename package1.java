3329dc20bfa5fb72146b305d2453a1b9e3f92c2d
ankush0520 <guptaankushkumar164@gmail.com>
2023-10-05 15:21:44 +0530

    For default access modifier User-defined package

---
+++ package1.java
@@ -0,0 +1,22 @@
+//User-Defined package
+
+package ankush;
+
+class A
+{
+	void show() //default access modifier
+	{
+		System.out.println("Learn Coding....");
+	}
+	
+}
+class B
+{
+	public static void main(String[] args)
+	{
+		A r=new A();
+		r.show();
+	}
+}
+//for compile we have to write "javac -d . class_name.java"
+//for run "java pacakge_name.class_name"
