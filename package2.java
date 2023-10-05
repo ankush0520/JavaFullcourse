5e9bc1fd3dccfdb17dc8423dfaf76904f92e8a31
ankush0520 <guptaankushkumar164@gmail.com>
2023-10-05 15:42:32 +0530

    For protected access modifier User-defined package

---
+++ package2.java
@@ -0,0 +1,25 @@
+//User-Defined package
+
+package ankush;
+
+public class A
+{
+	protected void show()
+	{
+		System.out.println("Learn Coding....");
+	}
+	
+}
+//for compile we have to write "javac -d . class_name.java"
+//for run "java pacakge_name.class_name"
+//new file c.java and new package protected is accessed outside the package(new package) with help of sub class
+package gupta;
+import ankush.*;
+class  c extends A
+{
+	public static void main(String[] args)
+	{
+		c r=new c();
+		r.show();
+	}
+}
