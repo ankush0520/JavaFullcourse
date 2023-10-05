238adac69ba877fbe30e22d3e57ca69b57063fae
ankush0520 <guptaankushkumar164@gmail.com>
2023-10-05 21:46:03 +0530

    Java Collections (Stack)

---
+++ JavaCollections2.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+class JavaCollections
+{
+	public static void main(String[] main)
+	{
+		Stack<String> Name=new Stack<>();  //predefined Stack class and it is LIFO(Last in First out)
+		Name.push("Ankush"); //adding 0th element in Stack
+		Name.push("Kumar");  //adding 1st element in Stack
+		Name.push("Gupta");  //adding 2nd element in Stack
+
+		System.out.println(Name);
+		Name.pop();             //pop will delete the element (LIFO)
+		System.out.println(Name);
+    }
+}
