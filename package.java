25ae631d68e06f1d54446e522da998acf1b79227
ankush0520 <guptaankushkumar164@gmail.com>
2023-10-05 14:52:36 +0530

    User-defined package

---
+++ package.java
@@ -0,0 +1,17 @@
+//User-Defined package
+package ankush;
+
+class A
+{
+	private void show()
+	{
+		System.out.println("Learn Coding....");
+	}
+	public static void main(String[] args)
+	{
+		A r=new A();
+		r.show();
+	}
+}
+//for compile we have to write "javac -d . class_name.java"
+//for run "java pacakge_name.class_name"
