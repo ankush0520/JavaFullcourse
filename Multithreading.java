6e1d485e4d1270082b798c84e4cc27b83d2011c8
ankush0520 <guptaankushkumar164@gmail.com>
2023-10-05 17:18:27 +0530

    By Extending thread class

---
+++ Multithreading.java
@@ -0,0 +1,34 @@
+                        /*By Extending thread class*/
+class A extends Thread
+{
+	@Override
+	public void run()
+	{
+		try
+		{
+			for(int i=0;i<5;i++)
+		    {
+			  System.out.println("Ankush");
+			  Thread.sleep(1000); 
+		    }
+		}
+		catch(InterruptedException i)
+		{
+
+		}
+		
+	}
+}
+class b
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		A t=new A();
+		t.start();
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("Ekram");
+			Thread.sleep(1000);
+		}
+	}
+}
