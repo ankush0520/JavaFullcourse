8c912c500de89ba2b9720ceea1c6b7b5e42e0da4
ankush0520 <guptaankushkumar164@gmail.com>
2023-10-05 16:21:30 +0530

    For public  access modifier User-defined package

---
+++ package4.java
@@ -0,0 +1,27 @@
+//User-Defined package
+
+package ankush;
+
+public class A
+{
+	public void show()
+	{
+		System.out.println("Learn Coding....!");
+	}
+	
+}
+//for compile we have to write "javac -d . class_name.java"
+//for run "java pacakge_name.class_name"
+//new file c.java and new package public is accessed outside the package any where
+package gupta;
+import ankush.*;
+class c
+{
+	public static void main(String[] args)
+	{
+		A r=new A();
+		r.show();
+	}
+}
+
+
